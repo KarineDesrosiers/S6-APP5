1763c97b97eccdae60242a557bc329cc2c2f56c6
Karine <karinedesrosiers@live.ca>
2022-07-09 16:01:27 -0400

    Relay done!

--- api/relay.js
+++ api/relay.js
@@ -1,14 +1,21 @@
+const Particle = require('particle-api-js');
+const particle = new Particle();
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://broker.hivemq.com');
+const token = '1cfa44ee31c23b8a47be328476a0a1fde985f04d';
+const topicEntered = 'desk2812-mail2605/events/entered';
+const topicLeft = 'desk2812-mail2605/events/left';
 
-var state = 'true';
-
-client.on('connect', () => {
-    client.publish('mail2605/connected', 'true');
-    sendStateUpdate();
+particle.getEventStream({name: 'Entered', auth: token}).then(function(stream) {
+    stream.on('event', function(data) {
+        console.log('Event: ', data);
+        client.publish(topicEntered, data.data);
+    });
 });
 
-function sendStateUpdate() {
-    console.log('sending state %s', state);
-    client.publish('mail2605/state', state);
-}
+particle.getEventStream({name: 'Left', auth: token}).then(function(stream) {
+    stream.on('event', function(data) {
+        console.log("Event: ", data);
+        client.publish(topicLeft, data.data);
+    });
+});
