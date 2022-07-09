b77306c3010a8e99547e91ffa912702791c04d59
Laurent <lbourassa95@gmail.com>
2022-07-09 17:00:02 -0400

    Expose toggle function

--- argon/src/argon.cpp
+++ argon/src/argon.cpp
@@ -9,9 +9,13 @@
 // SYSTEM_THREAD(ENABLED);
 
 void onCallback(Beacon& beacon, callback_type type);
+PinState toggleLED(String command);
 void setup();
 void loop();
 #line 5 "c:/Users/Laurent/Documents/UdeS/S6/APP5/repo/argon/src/argon.ino"
+uint8_t ledPin = D7;
+PinState ledState = LOW;
+
 void onCallback(Beacon& beacon, callback_type type)
 {
   if(NEW == type)
@@ -27,10 +31,25 @@ void onCallback(Beacon& beacon, callback_type type)
   }
 }
 
+PinState toggleLED(String command)
+{
+  if(LOW == ledState) ledState = HIGH;
+  else ledState = LOW;
+
+  digitalWrite(ledPin, ledState);
+
+  return ledState;
+}
+
 void setup()
 {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
   Serial.begin();
   waitFor(Serial.isConnected, 30000);
+
+  Particle.function("toggle", toggleLED);
 
   Scanner.setCallback(onCallback);
   Scanner.setScanPeriod(1);
--- argon/src/argon.ino
+++ argon/src/argon.ino
@@ -2,6 +2,9 @@
 
 // SYSTEM_THREAD(ENABLED);
 
+uint8_t ledPin = D7;
+PinState ledState = LOW;
+
 void onCallback(Beacon& beacon, callback_type type)
 {
   if(NEW == type)
@@ -17,10 +20,25 @@ void onCallback(Beacon& beacon, callback_type type)
   }
 }
 
+PinState toggleLED(String command)
+{
+  if(LOW == ledState) ledState = HIGH;
+  else ledState = LOW;
+
+  digitalWrite(ledPin, ledState);
+
+  return ledState;
+}
+
 void setup()
 {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
   Serial.begin();
   waitFor(Serial.isConnected, 30000);
+
+  Particle.function("toggle", toggleLED);
 
   Scanner.setCallback(onCallback);
   Scanner.setScanPeriod(1);
