3fc8ff70bfba079d992167ffece83094c1d5c836
Karine <karinedesrosiers@live.ca>
2022-07-10 00:16:34 -0400

    GET/POST done!

--- api/archive.js
+++ api/archive.js
@@ -3,6 +3,13 @@ const client = mqtt.connect('mqtt://broker.hivemq.com');
 const topicEntered = 'desk2812-mail2605/events/entered';
 const topicLeft = 'desk2812-mail2605/events/left';
 const fs = require('fs');
+const file = 'database.txt';
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
 
 client.on('connect', () => {
     eraseContentFile();
@@ -12,27 +19,51 @@ client.on('connect', () => {
 
 client.on('message', (topic, message) => {
     if(topic === topicEntered) {
-        writeContentFile('Entered: ' + message.toString() + '\n\r');
+        writeContentFile('Entered ' + message.toString());
     }
     if(topic === topicLeft) {
-        writeContentFile('Left: ' + message.toString() + '\n\r');
+        writeContentFile('Left ' + message.toString());
     }
 });
 
+// https://nodejs.dev/learn/writing-files-with-nodejs
 function writeContentFile(content) {
-    fs.appendFile('database.txt', content, err => {
-        if(err) {
-            console.log('Error: ', err);
+    fs.appendFile(file, content + '\n', err => {
+        if (err) {
+            console.log('Error appendFile(): ', err);
         }
         console.log('File written successfully!');
     });
 }
 
 function eraseContentFile() {
-    fs.writeFile('database.txt', '', err => {
-        if(err) {
-            console.log('Error: ', err);
+    fs.writeFile(file, '', err => {
+        if (err) {
+            console.log('Error writeFile(): ', err);
         }
         console.log('File emptied successfully!');
     });
 }
+
+// https://nodejs.dev/learn/reading-files-with-nodejs
+app.get('/api/events', function(req, res) {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Error readFile(): ', err);
+            return;
+        }
+
+        var rows = data.split('\n');
+        var parsedData = [];
+        for (let i = 0; i < rows.length-1; i++) {
+            const element = rows[i].split(' ');
+            parsedData[i] = {name: element[0], address: element[1]};
+        }
+
+        res.json(parsedData);
+    });
+});
+
+app.listen(port, () => {
+    console.log('Archive server started!');
+});
--- api/control.js
+++ api/control.js
@@ -1,14 +1,27 @@
-const axios = require('axios');
 const Particle = require('particle-api-js');
-const particle = new Particle();
 const device = 'e00fce68afe7a9c5c32421cf';
 const token = '1cfa44ee31c23b8a47be328476a0a1fde985f04d';
 
-var res = particle.callFunction({ deviceId: device, name: 'toggle', auth: token });
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+app.use(cors());
 
-res.then(
-    function(data) {
-      console.log('Function called successfully:', data);
-    }, function(err) {
-      console.log('An error occurred:', err);
-    });
+app.post('/api/led', function(req, res) {
+    const particle = new Particle();
+
+    res = particle.callFunction({ deviceId: device, name: 'toggle', auth: token });
+    res.then(
+        function(data) {
+            console.log('Function called successfully:', data);
+            return res.sendStatus(200);
+        }, function(err) {
+            console.log('An error occurred:', err);
+            return res.sendStatus(500);
+        });
+});
+
+app.listen(port, () => {
+    console.log('Control server started!')
+});
